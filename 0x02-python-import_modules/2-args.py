936f3378f8e9d36267fd6d2ae3973643c729b7ca
Taoheed <oyeniyiemperor@gmail.com>
2022-08-25 14:12:28 +0100

    Modified: 0-add.py, 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,8 @@ if __name__ == "__main__":
 
     if a == 0:
         print("{} arguments.".format(a))
+    elif a == 1:
+        print("{} argument:".format(a))
     else:
         print("{} arguments:".format(a))
 
