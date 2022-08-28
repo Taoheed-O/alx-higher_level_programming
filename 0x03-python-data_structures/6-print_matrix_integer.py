656cc122ac015035eb46f917b4992016078dcf32
Taoheed <oyeniyiemperor@gmail.com>
2022-08-28 02:24:18 +0100

    Added: 6-print_matrix_integer.py - prints integers in a mtrix like list.

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    length = len(matrix)
+    for i in range(length):
+        for j in range(len(matrix[i])):
+            if j != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[i][j]), end='')
+        print()
