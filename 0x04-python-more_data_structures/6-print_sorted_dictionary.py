4982ff87d7e44dfedb0f6b4a6c5672f9b7f41f3c
Taoheed <oyeniyiemperor@gmail.com>
2022-09-01 02:54:20 +0100

    Added: 7-update_dictionary.py - updates a dictionary.

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    dict_keys = []
-    for key, value in a_dictionary.items():
-        dict_keys.append(key)
-    dict_keys = sorted(dict_keys)
-    for key in dict_keys:
-        print("{}: {}".format(key, a_dictionary.get(key)))
+    list_ord = list(a_dictionary.keys())
+    list_ord.sort()
+    for i in list_ord:
+        print("{}: {}".format(i, a_dictionary.get(i)))
