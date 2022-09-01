a2cf21f3b8fc221617b709186a089155eb17c197
Taoheed <oyeniyiemperor@gmail.com>
2022-09-01 02:27:41 +0100

    Added: 6-print_sorted_dictionary.py - prints a dictionary by keys sorted alphabetically with importing any modules.

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -5,4 +5,4 @@ def print_sorted_dictionary(a_dictionary):
         dict_keys.append(key)
     dict_keys = sorted(dict_keys)
     for key in dict_keys:
-        print("{}: {}".format(i, a_dictionary.get(i)))
+        print("{}: {}".format(key, a_dictionary.get(key)))
